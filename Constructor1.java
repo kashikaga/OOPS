e05b454b1682768a99f3a20eb8a87b62f92646c4
KGupta2601 <gkashika26@gmail.com>
2025-01-03 22:48:10 +0530

    added Constructor overloading

--- Constructor1.java
+++ Constructor1.java
@@ -1,7 +1,9 @@
 public class Constructor1{
+    //constructor overloading
     public static void main(String[] args) {
         St1 s1= new St1();
-      // System.out.println(s1.name);
+        St1 s2= new St1("kashika");
+        St1 s3= new St1(10);
     }
 }
 
@@ -11,5 +13,11 @@ class St1 {
 
     St1(){
         System.out.println("constructor is called..");
+    }
+    St1(String name){
+        this.name=name;
+    }
+    St1(int roll){
+        this.roll=roll;
     }
 }
